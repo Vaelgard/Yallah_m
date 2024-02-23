7ddfd09a7c21b4fc1cc87ee5d63ba11d3fa606b6
ymeriaf5 <87639959+ymeriaf5@users.noreply.github.com>
2024-02-23 22:39:47 +0100

    first commit

---
+++ src/main/java/com/example/yallah_m/YallahMApplication.java
@@ -0,0 +1,13 @@
+package com.example.yallah_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallahMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YallahMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yallah_m/YallahMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yallah_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallahMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
