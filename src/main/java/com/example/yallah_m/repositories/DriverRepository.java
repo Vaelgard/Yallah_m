57c03a9ffd81c8e87043e743c7cd89d3b220a17e
ymeriaf5 <87639959+ymeriaf5@users.noreply.github.com>
2024-03-05 19:40:00 +0100

    first commit

--- src/main/java/com/example/yallah_m/repositories/DriverRepository.java
+++ src/main/java/com/example/yallah_m/repositories/DriverRepository.java
@@ -4,4 +4,5 @@ import com.example.yallah_m.entities.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DriverRepository extends JpaRepository<Driver,String> {
+    Driver findDriverByCIN(String cin);
 }
